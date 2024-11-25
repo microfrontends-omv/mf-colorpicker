6fe795c3d9ddcc3a068489f35e41a91018744c37
Oscar Morlet <oscarmorletdev@gmail.com>
2024-11-24 23:59:00 -0600

    build: change production URL in webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ const printCompilationMessage = require('./compilation.config.js');
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3001/",
+    // publicPath: "http://localhost:3001/",
+    publicPath: "https://mf-colorpicker-omv.netlify.app",
   },
 
   resolve: {
